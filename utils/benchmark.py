b3de54724008ac637affdff6ec08ba164c49f7c6
Adrian Arimany <176186615+adrianArimany@users.noreply.github.com>
2025-09-05 17:47:05 -0600

    added a benchmark to compare monte carlo and matrix product.

---
+++ utils/benchmark.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from simulation.monte_carlo import simulate_monte_carlo
+from simulation.matrix_method import simulate_matrix_method
+
+def benchmark_pfa(pfa, words, n_trial=1000):
+    """_summary_
+
+    Args:
+        pfa (_type_): _description_
+        words (_type_): _description_
+        n_trial (int, optional): _description_. Defaults to 1000.
+    """
+    mc_result = simulate_monte_carlo(pfa, words[0], n_trial=n_trial)
+    mm_result = simulate_matrix_method(pfa, words[0])
+    
+    
+    df = pd.DataFrame([
+    {
+    "Method": "Monte Carlo",
+    "Probability": mc_result["acceptance_probability"],
+    "Average Path Prob": mc_result["average_path_probability"],
+    "Stddev Path Prob": mc_result["stddev_path_probability"],
+    "Elapsed Time (s)": mc_result["elapsed_time"],
+    "Trials": mc_result["n_trials"]
+    },
+    {
+    "Method": "Matrix Product",
+    "Probability": mm_result.exact_result.get("exact_probability", 0.0),
+    "Average Path Prob": None,
+    "Stddev Path Prob": None,
+    "Elapsed Time (s)": mm_result.exact_result["elapsed_time"],
+    "Trials": None
+    }
+    ])
+    
+    return df
+
